7716b2472f87e6b14857c4cd7c45749ac709bde3
Grzesiek <data.grzegorz@gmail.com>
2019-11-18 21:51:08 +0100

    disuq commite

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import DefaultLayout from "~/layouts/Default.vue";
 import settings from "../data/theme.json";
-
+import VueDisqus from 'vue-disqus'
 import "@/assets/code-highlight.css"
 
 export default function(Vue, { head }) {
+  Vue.use(VueDisqus)
   Vue.component("Layout", DefaultLayout);
   head.bodyAttrs = {
     class: settings.dark_mode ? "dark" : ""
