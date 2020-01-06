37ccb4c043c8b32eaf534794a66709f90b6f7345
Grzesiek <data.grzegorz@gmail.com>
2020-01-06 22:17:32 +0100

    10

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import "@/assets/code-highlight.css"
 export default function(Vue, { head }) {
   Vue.use(VueDisqus)
   Vue.component("Layout", DefaultLayout);
+  head.htmlAttrs = { lang: 'pl' }
   head.bodyAttrs = {
     class: settings.dark_mode ? "dark" : ""
   };
